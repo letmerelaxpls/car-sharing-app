a3ca775247c960094076415802ababb4702f1029
Takeit <larsubivec@gmail.com>
2025-07-22 15:35:31 +0400

    infrastructure commit

---
+++ src/test/java/car_sharing_app/CarSharingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package car_sharing_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
